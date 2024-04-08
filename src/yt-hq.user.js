1f28aa0bb3d6b6a6b0aad2caa8ddd11f449659fb
Pedro Américo <pedroamericolouzada@uol.com.br>
2024-04-08 18:55:24 -0300

    [yt-hq]: fix for in-app navigation issue

--- src/yt-hq.user.js
+++ src/yt-hq.user.js
@@ -4,24 +4,31 @@
 // @match       https://www.youtube.com/watch*
 // @grant       none
 // @require		https://raw.githubusercontent.com/ghostebony/userscripts/main/src/utils.js
-// @version     0.0.1
+// @version     0.0.2
 // @author      ghostebony
 // @description set the highest video quality possible
 // @downloadURL https://raw.githubusercontent.com/ghostebony/userscripts/main/src/yt-hq.user.js
 // ==/UserScript==
 
-waitForElement('button.ytp-hd-quality-badge').then((settings) => {
-	settings.click();
-
-	waitForElement('div.ytp-panel-menu > div.ytp-menuitem[role="menuitem"]:last-child').then(
-		(quality) => {
-			quality.click();
+waitForElement('title').then((title) => {
+	const observer = new MutationObserver(() => {
+		waitForElement('button.ytp-hd-quality-badge').then((settings) => {
+			settings.click();
 
 			waitForElement(
-				'div.ytp-panel-menu > div.ytp-menuitem[role="menuitemradio"]:first-child',
-			).then((highestQuality) => {
-				highestQuality.click();
+				'div.ytp-panel-menu > div.ytp-menuitem[role="menuitem"]:last-child',
+			).then((quality) => {
+				quality.click();
+
+				waitForElement(
+					'div.ytp-panel-menu > div.ytp-menuitem[role="menuitemradio"]:first-child',
+				).then((highestQuality) => {
+					highestQuality.click();
+
+				});
 			});
-		},
-	);
+		});
+	});
+
+	observer.observe(title, { childList: true });
 });
