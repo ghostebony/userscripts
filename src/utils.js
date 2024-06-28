73b6a929d6c8833e8d824f27a16abe016772236d
Pedro Américo <pedroamericolouzada@uol.com.br>
2024-06-28 14:06:24 -0300

    [utils]: make request options optional

--- src/utils.js
+++ src/utils.js
@@ -52,7 +52,7 @@ async function hash(string, hash = 'SHA-1') {
  * @template {Record<string, unknown> | Array<Record<string, unknown>> | string[] | number[] | null} TData
  *
  * @param {string} url
- * @param {{method?: string}} options
+ * @param {{method?: string}} [options]
  *
  * @return {Promise<{data: TData; status: number;}>}
  */
@@ -60,7 +60,7 @@ async function request(url, options) {
 	return new Promise((resolve, reject) => {
 		GM_xmlhttpRequest({
 			url,
-			method: options.method ?? 'GET',
+			method: options?.method ?? 'GET',
 			onabort: reject,
 			onerror: reject,
 			onload({ status, responseText }) {
